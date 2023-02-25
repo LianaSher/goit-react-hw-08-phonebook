dc8a0da9fb4b52dfc67dba854f0719fbf95e5b41
LianaSher <sheremetliana@gmail.com>
2023-02-25 22:33:15 +0200

    correction

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,64 +1,73 @@
-import { Component } from "react";
+import { Component } from 'react';
 import { nanoid } from 'nanoid';
-import { GlobalStyle } from "GlobalStales";
+import { GlobalStyle } from 'GlobalStales';
 
-import { Filter } from "../components/Filter/Filter";
-import { Contacts } from "../components/Contacts/Contacts";
-import { Form } from "./Form/Form";
+import { Filter } from '../components/Filter/Filter';
+import { Contacts } from '../components/Contacts/Contacts';
+import { Form } from './Form/Form';
 
-import {Container,Title} from "./App.styled";
+import { Container, Title } from './App.styled';
 
 export class App extends Component {
-state = {
-   contacts: [],
-   filter: "",
-  }
-  
-  addContactToState = ({ name, number }) => {
-    this.setState((prevState) => {
-      const { contacts } = prevState;
-      if(this.isContactExist(name)) { return alert(`${name} is already in Contacts`)}
-      const newContact = { name, id: nanoid(), number};
-      return {contacts: [newContact, ...contacts]}
-      
-    })
-  }
+  state = {
+    contacts: [],
+    filter: '',
+  };
 
- isContactExist = (newName) =>{
+  addContactToState = ({ name, number }) => {
+    this.setState(prevState => {
+      const { contacts } = prevState;
+      if (this.isContactExist(name)) {
+        return alert(`${name} is already in Contacts`);
+      }
+      const newContact = { name, id: nanoid(), number };
+      return { contacts: [newContact, ...contacts] };
+    });
+  };
+
+  isContactExist = newName => {
     const { contacts } = this.state;
-    return contacts.find(({name})=>{ return(name.toLowerCase() === newName.toLowerCase())})
-  }
-  
-  removeContact = (idForDelete) => {
-      this.setState(({ contacts }) => {
-      const newContacts = contacts.filter(({ id }) => id !== idForDelete)
-      return { contacts: newContacts }
-    })}
-  
+    return contacts.find(({ name }) => {
+      return name.toLowerCase() === newName.toLowerCase();
+    });
+  };
+
+  removeContact = idForDelete => {
+    this.setState(({ contacts }) => {
+      const newContacts = contacts.filter(({ id }) => id !== idForDelete);
+      return { contacts: newContacts };
+    });
+  };
+
   filterNames = () => {
     const { contacts, filter } = this.state;
     if (!filter) return contacts;
     return contacts.filter(({ name }) => {
-      return (name.toLowerCase().includes(filter.toLowerCase()))
+      return name.toLowerCase().includes(filter.toLowerCase());
     });
-    
-  }
-  
-  filterHandle = ({target}) => {
+  };
+
+  filterHandle = ({ target }) => {
     this.setState({ filter: target.value });
-  }
+  };
 
   render() {
     const filtredContacts = this.filterNames();
-    
-    return (<Container>
-      <Title>Phonebook</Title>
-      <Form onSubmit={this.addContactToState} />
-      <Title>Contacts</Title>
-      <Filter onChange={this.filterHandle}  />
-      <Contacts contacts={filtredContacts} onClick={this.removeContact} />
-      <GlobalStyle/>
-    </Container>)
+
+    return (
+      <Container>
+        <Title>Phonebook</Title>
+        <Form onSubmit={this.addContactToState} />
+        <Title>Contacts</Title>
+        <Filter onChange={this.filterHandle} />
+        {filtredContacts.length <= 0 ? (
+          <p>There are no contacts yet</p>
+        ) : (
+          <Contacts contacts={filtredContacts} onClick={this.removeContact} />
+        )}
+
+        <GlobalStyle />
+      </Container>
+    );
   }
 }
-
--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -1,17 +1,24 @@
-import PropTypes from "prop-types";
-import { Item } from "./ContactsItem";
+import PropTypes from 'prop-types';
+import { Item } from './ContactsItem';
 
 export const Contacts = ({ contacts, onClick }) => {
-    if (contacts.length <= 0) { return (<p>There are no contacts yet</p>) }
-    return (
-        <ol>{contacts.map(({ id, name, number }) => <li key={id}><Item id={id} name={name} number={number} onClick={onClick} /> </li>)} </ol>
-    )
-}
+  return (
+    <ol>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id}>
+          <Item id={id} name={name} number={number} onClick={onClick} />{' '}
+        </li>
+      ))}{' '}
+    </ol>
+  );
+};
 
 Contacts.propTypes = {
-    contacts: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.string,
-        name: PropTypes.string,
-        number: PropTypes.string,
-    })).isRequired,
-}
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      number: PropTypes.string,
+    })
+  ).isRequired,
+};
