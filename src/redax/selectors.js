1a11355464717868878e355d1a914c58f9515030
LianaSher <sheremetliana@gmail.com>
2023-03-31 15:05:42 +0300

    delete redax-persist

--- src/redax/selectors.js
+++ src/redax/selectors.js
@@ -1,13 +1,3 @@
 export const getContacts = state => state.contacts;
 
 export const getFilter = state => state.filter;
-
-// export const getFilteredContacts = state => {
-//   const { filter } = state.filter;
-
-//   if (!filter) return state.contacts;
-
-//   return state.contacts.filter(({ name }) => {
-//     return name.toLowerCase().includes(filter.toLowerCase());
-//   });
-// };
--- src/redax/store.js
+++ src/redax/store.js
@@ -1,25 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { combineReducers } from '@reduxjs/toolkit';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 
-const persistConfig = {
-  key: 'stateContacts',
-  storage,
-  blacklist: ['filter'],
-};
-
-const rootReducer = combineReducers({
-  contacts: contactsReducer,
-  filter: filterReducer,
-});
-
-export const persistedReducer = persistReducer(persistConfig, rootReducer);
-
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer,
+  },
 });
-
-export const persistor = persistStore(store);
